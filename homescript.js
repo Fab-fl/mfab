4df02eb7591451e5978e6b07a926b0f558ec3377
Fab-fl <90956803+Fab-fl@users.noreply.github.com>
2022-12-22 18:52:31 +0100

    grgr

--- homescript.js
+++ homescript.js
@@ -1,5 +1,4 @@
 function changeColor() {
-    // Get the selected color value from the form element
     var color = document.getElementById('color-picker').value;
     document.documentElement.style.setProperty('--accent-color', color);
   }
